9a6f1f53813f4eb19a869e643b42c9a915d902c0
Maverick <spacewave111@gmail.com>
2023-12-15 16:19:55 +0500

    Добавлена аватарка в таблице Accounts

--- Gallery/users/admin.py
+++ Gallery/users/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from .models import *
+from django.utils.safestring import mark_safe
 
 
 class AccountAdmin(admin.ModelAdmin):
-    list_display = ['user']
+    list_display = ['user', 'nickname', 'account_image_thumb']
     list_filter = ['user']
+
+    def account_image_thumb(self, obj):
+        return mark_safe('<a href="{}"><img src="{}" width="auto" height="50" /></a>'.format(obj.account_image.url, obj.account_image.url))
+
+    account_image_thumb.short_description = 'Avatar'  # Миниатюрка аватара
 
 
 admin.site.register(Account, AccountAdmin)
