0e897d6ba26f033ca78b94bc152a9b42d93b97c6
Maverick <spacewave111@gmail.com>
2023-12-24 15:07:08 +0500

    Отображаются теги и категория в image_detail.html. Удалены лишние файлы из приложения main.

--- Gallery/main/urls.py
+++ Gallery/main/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-                  path('', views.index, name='home'),
-                  path('image/<int:image_number>/', views.image_detail, name='main_image_detail'),
-                  path('top_authors/', views.top_authors, name='top_authors'),
+                  # path('', views.index, name='home'),
+                  # path('image/<int:image_number>/', views.image_detail, name='main_image_detail'),
+                  # path('top_authors/', views.top_authors, name='top_authors'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
