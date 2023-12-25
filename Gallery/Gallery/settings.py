9724b63cc8bd1c9646c59dcac0a704e3728c165c
Maverick <spacewave111@gmail.com>
2023-12-25 11:08:15 +0500

    STATICFILES_DIRS

--- Gallery/Gallery/settings.py
+++ Gallery/Gallery/settings.py
@@ -127,8 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "main/static"),
+# ]
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "main/static"),
+    BASE_DIR / "static",
+    "main/static/",
 ]
 
 MEDIA_URL = 'media/'
